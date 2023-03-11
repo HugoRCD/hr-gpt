d054916988dcfd95958d7070e1e45c443c77047c
HugoRCD <hugo.richard@epitech.eu>
2023-03-11 18:19:03 +0100

    Format chat

--- pages/app/chat.vue
+++ pages/app/chat.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { PaperAirplaneIcon } from "@heroicons/vue/24/outline";
-import {Chat} from "~/types/Chat";
-import {useErrorToast} from "~/composables/useToasts";
+import { Chat } from "~/types/Chat";
+
 definePageMeta({
   name: "Chat",
   title: "Chat",
   path: "/",
 });
 
-const chat = ref<Chat[]>([]);
+const chat = ref<Chat[]>([
+  {
+    role: "assistant",
+    content: "Salut comment je peux t'aider aujourd'hui ?",
+  },
+]);
 
 const loading = ref(false);
 
@@ -21,16 +26,15 @@ async function sendMessage() {
     chat.value.push({
       role: "user",
       content: message.value,
-    })
+    });
     const { data } = await useFetch("/api/chat", {
       method: "POST",
       body: {
         chat: chat.value,
-        message: message.value,
-      }
+      },
     });
     if (data.value) {
-      chat.value = data.value.body.chat;
+      chat.value = data.value.chat;
     }
     message.value = "";
     loading.value = false;
@@ -45,26 +49,27 @@ async function sendMessage() {
 <template>
   <div class="relative h-screen flex flex-col">
     <div class="p-4 flex-grow overflow-y-auto flex flex-col gap-4">
-      <div v-for="msg in chat" :key="msg.id" :class="msg.role === 'assistant' ? 'text-primary text-left' : 'text-primary text-right'">
-        <div class="p-4 bg-secondary rounded-xl shadow-xl inline-block">{{ msg.content }}</div>
+      <div v-for="msg in chat" :key="msg.id" class="flex flex-col"
+           :class="msg.role === 'assistant' ? 'items-start' : 'items-end'">
+        <div class="card-message p-4 bg-secondary rounded-xl shadow-xl text-sm inline-block max-w-xl"
+             v-if="msg.role !== 'system'">
+          <pre><code v-html="msg.content" class="break-words whitespace-pre-wrap"></code></pre>
+        </div>
       </div>
       <div class="flex justify-center items-center" v-if="loading">
         <img src="/loader.gif" alt="Loading..." class="h-24 w-24" />
       </div>
     </div>
-    <form class="bg-secondary w-full h-26 md:h-40 p-4 flex items-center flex-shrink-0" @submit.prevent="sendMessage" style="position: sticky; bottom: 0;">
+    <form class="bg-secondary sticky bottom-0 w-full h-26 md:h-40 p-4 flex items-center flex-shrink-0" @submit.prevent="sendMessage">
     <textarea v-model="message"
               @keydown.enter.prevent="sendMessage"
               :disabled="loading"
               class="w-full bg-primary p-4 text-primary border-none outline-none shadow-xl border-2 border-accent rounded-xl"
-              placeholder="Type your message here..." />
+              placeholder="Ecrivez votre message ici..."></textarea>
       <button type="submit" :disabled="loading">
-        <PaperAirplaneIcon class="text-accent cursor-pointer h-8 w-8 ml-4 hover:scale-125 transform transition duration-200 ease-in-out" />
+        <PaperAirplaneIcon
+          class="text-accent cursor-pointer h-8 w-8 ml-4 hover:scale-125 transform transition duration-200 ease-in-out" />
       </button>
     </form>
   </div>
-
 </template>
-
-<style scoped>
-</style>
--- server/api/chat.ts
+++ server/api/chat.ts
@@ -9,21 +9,18 @@ const configuration = new Configuration({
 
 export default eventHandler(async (event: H3Event) => {
   const body = await readBody(event);
-  const { chat, message } = body;
+  const { chat } = body;
   const openAiApi = new OpenAIApi(configuration);
   const response = await openAiApi.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: chat,
     temperature: 0.9,
-    stream: true,
+    stream: false,
     stop: ["user", "assistant"],
   });
-  console.log(response.data);
   const ia_response = response.data.choices[0].message;
   return {
     statusCode: 200,
-    body: {
-      chat: [...chat, ia_response],
-    }
+    chat: [...chat, ia_response],
   }
 });
